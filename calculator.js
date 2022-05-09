64bb27c440e2196ad7d3eceb82f0df2666e97e89
Xante Graham <xantegraham.14@gmail.com>
2022-05-09 16:45:17 -0500

    initial commit

---
+++ calculator.js
@@ -0,0 +1,5 @@
+const multiply = () => {
+  num1 = document.getElementById("num1").value;
+  num2 = document.getElementById("num2").value;
+  document.getElementById("result").innerHTML = num1 * num2;
+};
